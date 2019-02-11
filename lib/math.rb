748b50a9b613c3bcddcb085539e3c9cdafb95ba9
Matthew Shin <mshin1995@gmail.com>
2019-02-11 23:58:48 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,5 +1,5 @@
 def addition(num1, num2)
-
+  num1 + num2
 end
 
 def subtraction(num1, num2)
@@ -15,9 +15,11 @@ def multiplication(num1, num2)
 end
 
 def modulo(num1, num2)
+
 end
 
 def square_root(num)
+
 end
 
 def order_of_operation(num1, num2, num3, num4)
